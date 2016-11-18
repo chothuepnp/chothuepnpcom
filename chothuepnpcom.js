1a72b4943936f6292495ef24c24d4a21aaf5dccc
chothuepnp <chothuepnp@gmail.com>
2016-11-18 14:25:54 +0700

    Update chothuepnpcom.js

--- chothuepnpcom.js
+++ chothuepnpcom.js
@@ -3,7 +3,7 @@ function chothuepnp() {
 		var body_element = document.getElementsByTagName('body')[0];
 		var selection;
 		selection = window.getSelection();
-		var pagelink = "<br /><br />CHO THUÊ PNP;
+		var pagelink = "<br /><br />CHO THUÊ PNP";
 		var copytext = pagelink;
 		var newdiv = document.createElement('div');
 		
